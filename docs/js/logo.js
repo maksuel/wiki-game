5cd67ae631068f30632597edfe0e5642a8cd7d9f
Acer Aspire 5100 <maksuel@gmail.com>
2018-05-04 12:10:49 +0200

    Remove docs folder

--- docs/js/logo.js
+++
@@ -1,9 +0,0 @@
-function logo(id) {
-
-    let body = jQuery(id);
-
-    this.fadeOut = function(callback) {
-        body.fadeOut(config.fadeOutDuration, callback);
-        return this;
-    };
-}
